90502657db4eb61107c58ec466ef78ed7e2ca92a
Michael Zarate <michael.zarate.ce@gmail.com>
2020-06-17 16:05:22 -0500

    Update main.tf

--- terraform-workspace/main.tf
+++ terraform-workspace/main.tf
@@ -4,8 +4,33 @@ provider "google" {
     zone = "us-central1-a"
 }
 
-resource "google_compute_instance" "vm_instance" {
-    name = "micro-ubuntu18"
+# resource "google_compute_instance" "vm_instance_01" {
+#     name = "ubuntu18-micro"
+#     machine_type = "f1-micro"
+
+#     tags = ["http-server", "https-server"]
+
+#     labels = {
+#         purpose = "learning"
+#     }
+
+#     boot_disk {
+#         initialize_params {
+#             image = "ubuntu-os-cloud/ubuntu-1804-lts"
+#         }
+#     }
+
+#     network_interface {
+#         network = "default"
+
+#         access_config {
+
+#         }
+#     }
+# }
+
+resource "google_compute_instance" "vm_instance_02" {
+    name = "rhel8-micro"
     machine_type = "f1-micro"
 
     tags = ["http-server", "https-server"]
@@ -16,7 +41,7 @@ resource "google_compute_instance" "vm_instance" {
 
     boot_disk {
         initialize_params {
-            image = "ubuntu-os-cloud/ubuntu-1804-lts"
+            image = "rhel-cloud/rhel-8"
         }
     }
 
