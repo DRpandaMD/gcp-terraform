ff529d4fa0dbd92bea9f516fe249060cfc958f75
DRpandaMD <michael.zarate.ce@gmail.com>
2020-06-24 11:07:30 -0500

    added code for a SUSE image deployment

--- terraform-workspace/main.tf
+++ terraform-workspace/main.tf
@@ -12,6 +12,35 @@ provider "google" {
     zone = "us-central1-a"
 }
 
+# a SUSE Enterprise Linux  Server Distro 
+
+resource "google_compute_instance" "sles_instance_01" {
+    name = "sles15-micro"
+    machine_type = "f1-micro"
+
+    tags = ["http-server", "https-server"]
+
+    labels = {
+        purpose = "learning"
+    }
+
+    boot_disk {
+        initialize_params {
+            image = "suse-cloud/sles-15"
+        }
+    }
+
+    network_interface {
+        network = "default"
+
+        access_config {
+
+        }
+    }
+}
+
+# a ubuntu distro to use
+
 /* resource "google_compute_instance" "ubuntu18_instance_01" {
     name = "ubuntu18-micro"
     machine_type = "f1-micro"
@@ -37,7 +66,7 @@ provider "google" {
     }
 } */
 
-resource "google_compute_instance" "rhel8_instance_01" {
+/* resource "google_compute_instance" "rhel8_instance_01" {
     name = "rhel8-micro"
     machine_type = "f1-micro"
 
@@ -60,7 +89,7 @@ resource "google_compute_instance" "rhel8_instance_01" {
 
         }
     }
-}
+}*/
 
 
 
